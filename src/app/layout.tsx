5a3eb59c6e36ba96719b4cbd321c5c7f36e2c246
Ramesh Bojanapu <84425972+Ramesh-Bojanapu-1011@users.noreply.github.com>
2025-04-01 12:21:43 +0530

    Update layout metadata keywords to include MERN Stack Developer and Tech Enthusiast and add nexjs in skills

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
     apple: "/Logo.png",
   },
   keywords:
-    "portfolio app,portfolio my, Entry-Level Software Engineer,Fresher Developer,Job-Seeking Developer,Open to Work,Available for Remote Work,Web Developer,Next.js Developer,Full Stack Developer,Frontend Developer",
+    "portfolio app,portfolio my, Entry-Level Software Engineer,Fresher Developer,Job-Seeking Developer,Open to Work,Available for Remote Work,Web Developer,Next.js Developer,Full Stack Developer,Frontend Developer,MERN Stack Developer,Tech Enthusiast",
   authors: [{ name: "Ramesh" }],
   verification: {
     google: "fs3fI4RT8B2j8VUznZ2Lcuuzim3ucHDUWh0rHgeUe54",
