7cd6d5af88a9aa996a0884d944d4ddf7f9eb2055
Ramesh Bojanapu <84425972+Ramesh-Bojanapu-1011@users.noreply.github.com>
2025-05-28 22:32:23 +0530

    style: standardize quotes to single quotes across all files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: "Ramesh's protfolio",
   description: "Ramesh's portfolio app",
   icons: {
-    icon: "/Logo.png",
-    apple: "/Logo.png",
+    icon: '/Logo.png',
+    apple: '/Logo.png',
   },
   keywords:
-    "portfolio app,portfolio my, Entry-Level Software Engineer,Fresher Developer,Job-Seeking Developer,Open to Work,Available for Remote Work,Web Developer,Next.js Developer,Full Stack Developer,Frontend Developer,MERN Stack Developer,Tech Enthusiast",
-  authors: [{ name: "Ramesh" }],
+    'portfolio app,portfolio my, Entry-Level Software Engineer,Fresher Developer,Job-Seeking Developer,Open to Work,Available for Remote Work,Web Developer,Next.js Developer,Full Stack Developer,Frontend Developer,MERN Stack Developer,Tech Enthusiast',
+  authors: [{ name: 'Ramesh' }],
   verification: {
-    google: "fs3fI4RT8B2j8VUznZ2Lcuuzim3ucHDUWh0rHgeUe54",
+    google: 'fs3fI4RT8B2j8VUznZ2Lcuuzim3ucHDUWh0rHgeUe54',
   },
 };
 
--- src/components/Landingpage.tsx
+++ src/components/Landingpage.tsx
@@ -1,83 +1,83 @@
 /* eslint-disable no-empty */
-"use client";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useCallback, useEffect, useRef, useState } from "react";
-import Typed from "typed.js";
-import ContactForm from "./ContactForm";
-import { BentoCard, BentoGrid } from "./ui/bento-grid";
+'use client';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import Typed from 'typed.js';
+import ContactForm from './ContactForm';
+import { BentoCard, BentoGrid } from './ui/bento-grid';
 
 const features = [
   {
     // Icon: FileTextIcon,
-    name: "Biodata for Marriage",
+    name: 'Biodata for Marriage',
     description:
-      "Designed and developed multiple customizable templates to efficiently share family and personal details for various use cases.",
-    href: "https://biodataformarriage.net/",
-    cta: "Learn more",
+      'Designed and developed multiple customizable templates to efficiently share family and personal details for various use cases.',
+    href: 'https://biodataformarriage.net/',
+    cta: 'Learn more',
     background: (
       <img
         className="absolute opacity-60"
         src="/biodataformarriage.jpg"
-        alt={""}
+        alt={''}
       />
     ),
     className:
-      "lg:row-start-1 lg:row-end-4 lg:col-start-2 h-[300px] lg:col-end-3",
+      'lg:row-start-1 lg:row-end-4 lg:col-start-2 h-[300px] lg:col-end-3',
   },
   {
     // Icon: InputIcon,
-    name: "RandomGenerator.AI",
+    name: 'RandomGenerator.AI',
     description:
-      "Welcome to RandomGenerator.AI, your one-stop source for all kinds of random data. We make it easy and fun to find random info for any purpose!",
-    href: "https://randomgenerator.ai/",
-    cta: "Learn more",
+      'Welcome to RandomGenerator.AI, your one-stop source for all kinds of random data. We make it easy and fun to find random info for any purpose!',
+    href: 'https://randomgenerator.ai/',
+    cta: 'Learn more',
     background: (
       <img
         className="absolute opacity-60"
         src="/randomgenerator.jpg"
-        alt={""}
+        alt={''}
       />
     ),
     className:
-      "lg:col-start-1 lg:col-end-2 lg:row-start-1 h-[300px] lg:row-end-3",
+      'lg:col-start-1 lg:col-end-2 lg:row-start-1 h-[300px] lg:row-end-3',
   },
   {
     // Icon: GlobeIcon,
-    name: "Dyuti AI - Tools for students to help in their pursuit of dreams",
+    name: 'Dyuti AI - Tools for students to help in their pursuit of dreams',
     description:
-      "Embark on a transformative journey with Dyuti AI as we strive to revolutionize the way students navigate through college admissions and counseling processes.",
-    href: "https://dyuti.ai/",
-    cta: "Learn more",
+      'Embark on a transformative journey with Dyuti AI as we strive to revolutionize the way students navigate through college admissions and counseling processes.',
+    href: 'https://dyuti.ai/',
+    cta: 'Learn more',
     background: (
-      <img className="absolute opacity-60" src="/dyuti.jpg" alt={""} />
+      <img className="absolute opacity-60" src="/dyuti.jpg" alt={''} />
     ),
     className:
-      "lg:col-start-1 lg:col-end-2 lg:row-start-3 h-[300px] lg:row-end-4",
+      'lg:col-start-1 lg:col-end-2 lg:row-start-3 h-[300px] lg:row-end-4',
   },
   {
     // Icon: CalendarIcon,
-    name: "5reels",
+    name: '5reels',
     description: `At 5Reels.com, we celebrate the magic of storytelling. Our goal is to connect Telugu movie lovers everywhere with their cultural roots through cinema.`,
-    href: "https://www.5reels.com/",
-    cta: "Learn more",
+    href: 'https://www.5reels.com/',
+    cta: 'Learn more',
     background: (
-      <img className="absolute opacity-60" src="/5reels.jpg" alt={""} />
+      <img className="absolute opacity-60" src="/5reels.jpg" alt={''} />
     ),
     className:
-      "lg:col-start-3 lg:col-end-3 lg:row-start-1 h-[300px] lg:row-end-2",
+      'lg:col-start-3 lg:col-end-3 lg:row-start-1 h-[300px] lg:row-end-2',
   },
   {
     // Icon: CalendarIcon,
-    name: "ChatConnect",
+    name: 'ChatConnect',
     description: `Users can register, login, and chat with their friends securely. The app supports direct messaging, read receipts, file sharing, and real-time updates. Conversations are stored securely and sorted by the latest message.`,
-    href: "https://chat-app-front-another.vercel.app/",
-    cta: "Learn more",
+    href: 'https://chat-app-front-another.vercel.app/',
+    cta: 'Learn more',
     background: (
-      <img className="absolute opacity-60" src="/chatconnect.jpg" alt={""} />
+      <img className="absolute opacity-60" src="/chatconnect.jpg" alt={''} />
     ),
     className:
-      "lg:col-start-3 lg:col-end-3 lg:row-start-1 h-[300px] lg:row-end-2",
+      'lg:col-start-3 lg:col-end-3 lg:row-start-1 h-[300px] lg:row-end-2',
   },
 ];
 
@@ -97,9 +97,9 @@ const Landingpage = () => {
     }
     return age;
   };
-  console.log(calculateAge("2000-11-10"));
+  console.log(calculateAge('2000-11-10'));
   const calcScrollValue = () => {
-    const scrollProgress = document.getElementById("progress") as HTMLElement; // Type assertion
+    const scrollProgress = document.getElementById('progress') as HTMLElement; // Type assertion
     const pos = document.documentElement.scrollTop;
 
     const calcHeight =
@@ -108,9 +108,9 @@ const Landingpage = () => {
     const scrollValue = Math.round((pos * 100) / calcHeight);
 
     if (pos > 100) {
-      scrollProgress.style.display = "grid";
+      scrollProgress.style.display = 'grid';
     } else {
-      scrollProgress.style.display = "none";
+      scrollProgress.style.display = 'none';
     }
 
     scrollProgress.style.background = `conic-gradient(#fff ${scrollValue}%, #e6006d ${scrollValue}%)`;
@@ -121,8 +121,8 @@ const Landingpage = () => {
     window.onload = calcScrollValue;
 
     // Add click listener once to avoid multiple bindings
-    const scrollProgress = document.getElementById("progress") as HTMLElement;
-    scrollProgress.addEventListener("click", () => {
+    const scrollProgress = document.getElementById('progress') as HTMLElement;
+    scrollProgress.addEventListener('click', () => {
       document.documentElement.scrollTop = 0;
     });
 
@@ -130,7 +130,7 @@ const Landingpage = () => {
     return () => {
       window.onscroll = null;
       window.onload = null;
-      scrollProgress.removeEventListener("click", () => {
+      scrollProgress.removeEventListener('click', () => {
         document.documentElement.scrollTop = 0;
       });
     };
@@ -146,16 +146,16 @@ const Landingpage = () => {
   }, []);
 
   useEffect(() => {
-    const menuLi = document.querySelectorAll("header ul li a");
-    const sections = document.querySelectorAll("section");
+    const menuLi = document.querySelectorAll('header ul li a');
+    const sections = document.querySelectorAll('section');
 
     const activeMenu = () => {
       let len = sections.length;
       while (--len && window.scrollY + 97 < sections[len].offsetTop) {}
 
-      menuLi.forEach((link) => link.classList.remove("active"));
+      menuLi.forEach((link) => link.classList.remove('active'));
       if (menuLi[len]) {
-        menuLi[len].classList.add("active");
+        menuLi[len].classList.add('active');
       }
     };
 
@@ -163,11 +163,11 @@ const Landingpage = () => {
     activeMenu();
 
     // Add event listener for scroll
-    window.addEventListener("scroll", activeMenu);
+    window.addEventListener('scroll', activeMenu);
 
     // Cleanup the event listener on component unmount
     return () => {
-      window.removeEventListener("scroll", activeMenu);
+      window.removeEventListener('scroll', activeMenu);
     };
   }, []);
 
@@ -177,7 +177,7 @@ const Landingpage = () => {
     AOS.init({ duration: 1000, delay: 100 });
     // Initialize Typed.js for typing effect
     const typed = new Typed(typedRef.current, {
-      strings: ["Designer", "Developer", "Freelancer", "Full Stack Developer"],
+      strings: ['Designer', 'Developer', 'Freelancer', 'Full Stack Developer'],
       typeSpeed: 100,
       backSpeed: 50,
       backDelay: 1000,
@@ -194,12 +194,12 @@ const Landingpage = () => {
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
-    document.body.classList.toggle("open", !isOpen);
+    document.body.classList.toggle('open', !isOpen);
   };
 
   const closeMenu = () => {
     setIsOpen(false);
-    document.body.classList.remove("open");
+    document.body.classList.remove('open');
   };
 
   return (
@@ -211,7 +211,7 @@ const Landingpage = () => {
           </a>
           {/*  */}
           <ul
-            className={`navlist ${isOpen ? "active" : ""}`}
+            className={`navlist ${isOpen ? 'active' : ''}`}
             onClick={closeMenu}
           >
             <li>
@@ -238,7 +238,7 @@ const Landingpage = () => {
               href="#contact"
               className="flex items-center gap-1 align-middle btn"
             >
-              Let&#39;s connect{" "}
+              Let&#39;s connect{' '}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="20"
@@ -258,7 +258,7 @@ const Landingpage = () => {
             </a>
             {/* <Navbar/> */}
             <div
-              className={`menu-icon ${isOpen ? "active" : ""}`}
+              className={`menu-icon ${isOpen ? 'active' : ''}`}
               onClick={toggleMenu}
             >
               <div className="bar"></div>
@@ -286,7 +286,7 @@ const Landingpage = () => {
 
             <div className="btn-box">
               <a href="mailto:bramesh101020@gmail.com" className="btn">
-                Hire Me Now !{" "}
+                Hire Me Now !{' '}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -301,7 +301,7 @@ const Landingpage = () => {
                 </svg>
               </a>
               <a href="img/resume.pdf" target="_blank" className="btn d-CV">
-                Download CV{" "}
+                Download CV{' '}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -424,13 +424,13 @@ const Landingpage = () => {
             />
             <div className="rotate-text">
               <div className="text">
-                {" "}
+                {' '}
                 <p>
-                  {text.split("").map((char, i) => (
+                  {text.split('').map((char, i) => (
                     <b
                       key={i}
                       style={{
-                        display: "inline-block",
+                        display: 'inline-block',
                         transform: `rotate(${i * 6.3}deg)`,
                       }}
                     >
@@ -450,8 +450,8 @@ const Landingpage = () => {
           <div className="about-img">
             <img src="img/aboutMe-preview.png" alt="" className="aboutHero " />
             <div className="showcase-ring">
-              <img src="shapes/ring.png" className="ring" alt={""} />
-              <img src="shapes/circle.png" className="circle" alt={""} />
+              <img src="shapes/ring.png" className="ring" alt={''} />
+              <img src="shapes/circle.png" className="circle" alt={''} />
             </div>
           </div>
           <div className="about-content">
@@ -483,7 +483,7 @@ const Landingpage = () => {
                         >
                           <path d="M192 128l128 128-128 128z"></path>
                         </svg>
-                        <strong className="px-1">Birthday:</strong>{" "}
+                        <strong className="px-1">Birthday:</strong>{' '}
                         <span>15 june 2001</span>
                       </li>
 
@@ -499,7 +499,7 @@ const Landingpage = () => {
                         >
                           <path d="M192 128l128 128-128 128z"></path>
                         </svg>
-                        <strong className="px-1">Phone:</strong>{" "}
+                        <strong className="px-1">Phone:</strong>{' '}
                         <a href="tel:9380898635">
                           <span>+91 9380898635</span>
                         </a>
@@ -516,7 +516,7 @@ const Landingpage = () => {
                         >
                           <path d="M192 128l128 128-128 128z"></path>
                         </svg>
-                        <strong className="px-1">City:</strong>{" "}
+                        <strong className="px-1">City:</strong>{' '}
                         <span>Rayachoty</span>
                       </li>
                     </ul>
@@ -534,9 +534,9 @@ const Landingpage = () => {
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path d="M192 128l128 128-128 128z"></path>
-                        </svg>{" "}
+                        </svg>{' '}
                         <strong className="px-1">Age:</strong>
-                        <span>{calculateAge("2001-06-15")}</span>
+                        <span>{calculateAge('2001-06-15')}</span>
                       </li>
                       <li className="flex items-center mb-5">
                         <svg
@@ -550,7 +550,7 @@ const Landingpage = () => {
                         >
                           <path d="M192 128l128 128-128 128z"></path>
                         </svg>
-                        <strong className="px-1">Degree:</strong>{" "}
+                        <strong className="px-1">Degree:</strong>{' '}
                         <span>Btech</span>
                       </li>
                       <li className="flex items-center mb-5">
@@ -582,7 +582,7 @@ const Landingpage = () => {
                         >
                           <path d="M192 128l128 128-128 128z"></path>
                         </svg>
-                        <strong className="px-1">Freelance:</strong>{" "}
+                        <strong className="px-1">Freelance:</strong>{' '}
                         <span>Available</span>
                       </li>
                     </ul>
@@ -731,15 +731,15 @@ const Landingpage = () => {
                 Contact Me
               </a>
             </div>
-            <img src="img/support.png" className="first" alt={""} />
+            <img src="img/support.png" className="first" alt={''} />
           </div>
 
           <div className="showcase">
-            <img src="shapes/ring.png" className="ring" alt={""} />
-            <img src="shapes/circle.png" className="circle" alt={""} />
-            <img src="shapes/circle.png" className="circle2" alt={""} />
-            <img src="shapes/circle.png" className="circle3" alt={""} />
-            <img src="shapes/half-ring.png" className="half-ring" alt={""} />
+            <img src="shapes/ring.png" className="ring" alt={''} />
+            <img src="shapes/circle.png" className="circle" alt={''} />
+            <img src="shapes/circle.png" className="circle2" alt={''} />
+            <img src="shapes/circle.png" className="circle3" alt={''} />
+            <img src="shapes/half-ring.png" className="half-ring" alt={''} />
           </div>
         </section>
 
@@ -763,14 +763,14 @@ const Landingpage = () => {
           </div>
 
           <div className="showcase">
-            <img src="shapes/ring.png" className="ring" alt={""} />
+            <img src="shapes/ring.png" className="ring" alt={''} />
             <img
               src="shapes/second-circle.png"
               className="second-circle"
-              alt={""}
+              alt={''}
             />
-            <img src="shapes/circle.png" className="circle" alt={""} />
-            <img src="shapes/half-ring.png" className="half-ring" alt={""} />
+            <img src="shapes/circle.png" className="circle" alt={''} />
+            <img src="shapes/half-ring.png" className="half-ring" alt={''} />
           </div>
         </section>
 
